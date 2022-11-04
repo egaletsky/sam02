d07589daace62aae248321d645e88065f371a2c1
egaletsky <galetsky@gmail.com>
2022-11-05 03:23:50 +0700

    yu

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -16,31 +16,21 @@ export const Counter = (props: CounterType) => {
             setCount(count + 1)           ///++count - так не стоит делать - нужно    count + 1 - потому что меняется стейт
         }
     }
+    const onReset = () => setCount(0)
 
-    const onReset = () => {
-        setCount(0)
-    }
 
     const classNameWindow = s.counterWindow + (count === countMax ? ' ' + s.redText : '')
 
     return (
         <div className={s.counter}>
 
-            <div className={classNameWindow}>
-                {'' + count + '/' + countMax}
-            </div>
+            <div className={classNameWindow}>{'' + count + '/' + countMax}</div>
 
             <div className={s.groupButton}>
-
-                <Button name={'inc'}
-                        callback={onIncrement}
-                        disabled={count === countMax}/>
-
-                <Button name={'reset'}
-                        callback={onReset}
-                        disabled={!count}/>
-
+                <Button name={'inc'} callback={onIncrement} disabled={count === countMax}/>
+                <Button name={'reset'} callback={onReset} disabled={!count}/>
             </div>
+
         </div>
     )
 
